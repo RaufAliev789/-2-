12920b77e99fbeb6411113d6450b026b05ec5346
Rauf <www.rauf09@gmail.com>
2024-02-25 17:57:41 +0300

    Добавьте файлы проекта.

---
+++ Лр 2 Основы Прог/Лр 2 Основы Прог.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double S_function(double N, double x) {
+	if  (N == 0)
+		return sqrt(x + 1);
+
+	else if (N >0)
+		return sqrt(x + N * int(S_function(N - 1, x)) );
+}
+
+int main() {
+	setlocale(LC_ALL, "Ru");
+
+	double N,  x, Res;
+	
+
+	cout << "Введите N: ";
+	cin >>  N;
+
+	cout << "Введите x: ";
+	cin >>  x;
+
+	Res = S_function(N, x);
+
+	cout << "Результат работы программы: " << Res << endl;
+
+	return 0;
+}
